2f55d3bf43f510ce39f5a9ac2a95f980346257af
KatieZoe <80615590+KatieZoe@users.noreply.github.com>
2024-01-04 15:01:06 +1100

    Schema created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_03_224805) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_04_032931) do
   create_table "employees", force: :cascade do |t|
     t.string "firstname"
     t.string "lastname"
